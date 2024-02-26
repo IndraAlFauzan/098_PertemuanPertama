0ded4b56e3b3defeb3bd82baa24929265414f926
IndraAlFauzan <39118928+IndraAlFauzan@users.noreply.github.com>
2024-02-26 15:15:57 +0700

    Add project files.

---
+++ PertemuanPertamaKelasB/PertemuanPertamaKelasB.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{//begin
+	// numeric nPanjang, nLebar, nLuas
+	// display 'Masukan Panjang= '
+	// accept nPanjang
+	// display 'Masukan Lebar = '
+	// accept nLebar
+	//compute nLuas = nPanjang * nLebar
+	// display 'Luasnya adalah = ' + nLuas
+
+
+
+}//end
